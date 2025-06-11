ba0ece9da002e1d19bf9ee071b8e04cea9780a77
bianca201098 <bianca.antoniv@escola.pr.gov.br>
2025-06-11 11:04:47 -0300

    Campo a Cidade

---
+++ sketch.js
@@ -0,0 +1,59 @@
+function setup () {
+  createCanvas(400, 400);
+}
+
+let xJogador = [0, 0, 0, 0, 0, 0];
+let yJogador = [115, 60, 190, 250, 310, 380];
+let jogador = ["🐄", "🏙", "🐓", "🏖","🐎","🏢"];
+let teclas = ["m","n","b","r","c","x"];
+let quantidade = jogador.length;
+
+function draw() {
+  ativaJogo();
+  desenhaJogadores();
+  desenhaLinhaDeChegada();
+  verificaVencedor();
+}
+
+function ativaJogo() {
+  if (focused == true) {
+    background("#4CAF50");
+  } else {
+    background("#4CAF50");
+  }
+}
+function desenhaJogadores() {
+  textSize(40);
+  for (let i = 0; i < quantidade; i++) {
+    text(jogador[i], xJogador[i], yJogador[i]);
+  }
+}
+
+function desenhaLinhaDeChegada() {
+  fill("white");
+  rect(350, 0, 10, 400);
+  fill("#795548");
+  for (let yAtual = 0; yAtual < 400; yAtual += 20) {
+    rect(350, yAtual, 10, 10);
+  }
+}
+
+function verificaVencedor() {
+  for (let i = 0; i < quantidade; i++) {
+    if (xJogador[i] > 350) {
+      text(jogador[i] + " 2 venceu!", 50, 200);
+      noLoop();
+    }
+  }
+}
+
+function keyReleased() {
+  for (let i = 0; i < quantidade; i++) {
+    if (key == teclas[i]) {
+      xJogador[i] += random(20);
+    }
+  }
+}
+
+
+
